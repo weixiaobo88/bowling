a41c044a16fdd7cfddee6fd353a8b308b7d56ceb
Zhaoyu <weixiaobo88@gmail.com>
2024-05-22 10:03:20 +0800

    format: reformat code

--- app/src/main/java/bowling/Frame.java
+++ app/src/main/java/bowling/Frame.java
@@ -1,41 +1,41 @@
 package bowling;
 
 public class Frame {
-  private static final int SCORE_OF_NO_THIRD_THROW = 0;
-  private static final int SCORE_OF_STRIKE = 10;
-  private static final int SCORE_OF_SPARE = 10;
-  private int firstThrow;
-  private int secondThrow;
-  private Integer thirdThrow;
+    private static final int SCORE_OF_NO_THIRD_THROW = 0;
+    private static final int SCORE_OF_STRIKE = 10;
+    private static final int SCORE_OF_SPARE = 10;
+    private int firstThrow;
+    private int secondThrow;
+    private Integer thirdThrow;
 
-  public Frame(int firstThrow, int secondThrow) {
-    this.firstThrow = firstThrow;
-    this.secondThrow = secondThrow;
-  }
+    public Frame(int firstThrow, int secondThrow) {
+        this.firstThrow = firstThrow;
+        this.secondThrow = secondThrow;
+    }
 
-  public Frame(int firstThrow, int secondThrow, Integer thirdThrow) {
-    this.firstThrow = firstThrow;
-    this.secondThrow = secondThrow;
-    this.thirdThrow = thirdThrow;
-  }
+    public Frame(int firstThrow, int secondThrow, Integer thirdThrow) {
+        this.firstThrow = firstThrow;
+        this.secondThrow = secondThrow;
+        this.thirdThrow = thirdThrow;
+    }
 
-  public boolean isStrike() {
-    return firstThrow == SCORE_OF_STRIKE || secondThrow == SCORE_OF_STRIKE;
-  }
+    public boolean isStrike() {
+        return firstThrow == SCORE_OF_STRIKE || secondThrow == SCORE_OF_STRIKE;
+    }
 
-  public int getScore() {
-    return firstThrow + secondThrow + (thirdThrow == null ? SCORE_OF_NO_THIRD_THROW : thirdThrow);
-  }
+    public int getScore() {
+        return firstThrow + secondThrow + (thirdThrow == null ? SCORE_OF_NO_THIRD_THROW : thirdThrow);
+    }
 
-  public int getFirstThrowScore() {
-    return firstThrow;
-  }
+    public int getFirstThrowScore() {
+        return firstThrow;
+    }
 
-  public boolean isSpare() {
-    return firstThrow + secondThrow == SCORE_OF_SPARE;
-  }
+    public boolean isSpare() {
+        return firstThrow + secondThrow == SCORE_OF_SPARE;
+    }
 
-  public int getThirdThrowScore() {
-    return thirdThrow == null ? SCORE_OF_NO_THIRD_THROW : thirdThrow;
-  }
+    public int getThirdThrowScore() {
+        return thirdThrow == null ? SCORE_OF_NO_THIRD_THROW : thirdThrow;
+    }
 }
--- app/src/test/java/bowling/GameTest.java
+++ app/src/test/java/bowling/GameTest.java
@@ -8,56 +8,56 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-  private static final int TOTAL_FRAMES = 10;
+    private static final int TOTAL_FRAMES = 10;
 
-  @Test
-  void test_all_strikes_with_11th_frame_10_and_12th_frame_10() {
-    Game game = new Game();
-    List<Frame> throwList = new ArrayList<>();
-    for (int i = 0; i < TOTAL_FRAMES; i++) {
-      if (i == TOTAL_FRAMES - 1) {
+    @Test
+    void test_all_strikes_with_11th_frame_10_and_12th_frame_10() {
+        Game game = new Game();
+        List<Frame> throwList = new ArrayList<>();
+        for (int i = 0; i < TOTAL_FRAMES; i++) {
+            if (i == TOTAL_FRAMES - 1) {
+                throwList.add(new Frame(10, 10, 10));
+            } else {
+                throwList.add(new Frame(10, 0));
+            }
+        }
+
+        int score = game.calculateScore(throwList);
+
+        assertEquals(300, score);
+    }
+
+    @Test
+    void test_strikes_with_some_normal_frames() {
+        Game game = new Game();
+        List<Frame> throwList = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            throwList.add(new Frame(10, 0));
+        }
+        throwList.add(new Frame(4, 5));
         throwList.add(new Frame(10, 10, 10));
-      } else {
-        throwList.add(new Frame(10, 0));
-      }
+
+        int score = game.calculateScore(throwList);
+
+        assertEquals(262, score);
     }
 
-    int score = game.calculateScore(throwList); 
-    
-    assertEquals(300, score);
-  }
+    @Test
+    void test_all_spares_with_11th_frame_5_and_12th_frame_5() {
+        Game game = new Game();
+        List<Frame> throwList = new ArrayList<>();
+        for (int i = 0; i < TOTAL_FRAMES; i++) {
+            if (i == TOTAL_FRAMES - 1) {
+                throwList.add(new Frame(5, 5, 5));
+            } else {
+                throwList.add(new Frame(5, 5));
+            }
+        }
 
-  @Test
-  void test_strikes_with_some_normal_frames() {
-    Game game = new Game();
-    List<Frame> throwList = new ArrayList<>();
-    for (int i = 0; i < 8; i++) {
-      throwList.add(new Frame(10, 0));
+        int score = game.calculateScore(throwList);
+
+        assertEquals(150, score);
     }
-    throwList.add(new Frame(4, 5)); 
-    throwList.add(new Frame(10, 10, 10)); 
-    
-    int score = game.calculateScore(throwList);
-
-    assertEquals(262, score);
-  }
-
-  @Test
-  void test_all_spares_with_11th_frame_5_and_12th_frame_5() {
-    Game game = new Game();
-    List<Frame> throwList = new ArrayList<>();
-    for (int i = 0; i < TOTAL_FRAMES; i++) {
-      if (i == TOTAL_FRAMES - 1) {
-        throwList.add(new Frame(5, 5, 5));
-      } else {
-        throwList.add(new Frame(5, 5));
-      }
-    }
-
-    int score = game.calculateScore(throwList);
-
-    assertEquals(150, score);
-  }
 
     @Test
     void test_all_normal_frames() {
@@ -72,22 +72,22 @@ class GameTest {
         assertEquals(80, score);
     }
 
-  @Test
-  void test_mixed_frames() {
-    Game game = new Game();
-    List<Frame> throwList = new ArrayList<>();
-    for (int i = 0; i < 5; i++) {
-      throwList.add(new Frame(10, 0));
-    }
-    for (int i = 0; i < 3; i++) {
-      throwList.add(new Frame(5, 5));
-    }
-    throwList.add(new Frame(4, 4));
-    throwList.add(new Frame(4, 4));
+    @Test
+    void test_mixed_frames() {
+        Game game = new Game();
+        List<Frame> throwList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            throwList.add(new Frame(10, 0));
+        }
+        for (int i = 0; i < 3; i++) {
+            throwList.add(new Frame(5, 5));
+        }
+        throwList.add(new Frame(4, 4));
+        throwList.add(new Frame(4, 4));
 
-    int score = game.calculateScore(throwList);
+        int score = game.calculateScore(throwList);
 
-    assertEquals(195, score);
-  }
+        assertEquals(195, score);
+    }
 
 }
